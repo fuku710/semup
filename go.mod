d310a237d94fe6b20aba89a3bec441ec7740b90b
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2021-07-13 00:27:58 +0900

    output tags

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
+	var r, err = git.PlainOpen("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tIter, err := r.Tags()
+	if err != nil {
+		log.Fatal(err)
+	}
+	tags := []string{}
+	err = tIter.ForEach(func(t *plumbing.Reference) error {
+		tags = append(tags, t.Name().Short())
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(tags)
 }
