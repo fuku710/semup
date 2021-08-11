d5c2bda55386ef36c6d11efc8421b3c96855a180
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2021-08-11 13:33:53 +0900

    add prompt to select next version

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module example.com/semup
 go 1.16
 
 require (
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
 	github.com/go-git/go-git/v5 v5.4.2 // indirect
+	github.com/manifoldco/promptui v0.8.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 )
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/manifoldco/promptui"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -20,13 +23,53 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	tags := []string{}
+	vs := []*semver.Version{}
 	err = tIter.ForEach(func(t *plumbing.Reference) error {
-		tags = append(tags, t.Name().Short())
+		v, err := semver.NewVersion(t.Name().Short())
+		if err == nil {
+			vs = append(vs, v)
+		}
 		return nil
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(tags)
+	sort.Sort(sort.Reverse(semver.Collection(vs)))
+	fmt.Println("Latest version:", vs[0])
+	prompt := promptui.Select{
+		Label: "Select next version",
+		Items: []string{"Patch", "Minor", "Major"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch result {
+	case "Patch":
+		v := vs[0].IncPatch()
+		fmt.Println(v)
+		createTag(*r, v.String())
+	case "Minor":
+		v := vs[0].IncMinor()
+		fmt.Println(vs[0].IncMinor())
+		createTag(*r, v.String())
+	case "Major":
+		v := vs[0].IncMajor()
+		fmt.Println(vs[0].IncMajor())
+		createTag(*r, v.String())
+	}
+}
+
+func createTag(r git.Repository, tag string) {
+	hRef, err := r.Head()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ref, err := r.CreateTag("v"+tag, hRef.Hash(), &git.CreateTagOptions{
+		Message: tag,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ref)
 }
